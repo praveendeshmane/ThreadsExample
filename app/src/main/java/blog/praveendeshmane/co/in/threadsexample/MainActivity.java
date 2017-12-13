744c9abc5c3a110d890dccfca6d2d6edde6ee931
praveendeshmane <deshmane.praveen@gmail.com>
2017-12-14 00:24:10 +0530

    Initial commit

---
+++ app/src/main/java/blog/praveendeshmane/co/in/threadsexample/MainActivity.java
@@ -0,0 +1,49 @@
+package blog.praveendeshmane.co.in.threadsexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private ProgressBar progress;
+    private TextView text;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        progress = findViewById(R.id.progressBar1);
+        text =  findViewById(R.id.textView1);
+
+    }
+
+    public void startProgress(View view) {
+        // do something long
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <= 10; i++) {
+                    final int value = i;
+                    doFakeWork();
+                    progress.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            text.setText("Updating");
+                            progress.setProgress(value);
+                        }
+                    });
+                }
+            }
+        };
+        new Thread(runnable).start();
+    }
+
+    // Simulating something timeconsuming
+    private void doFakeWork() {
+        SystemClock.sleep(1000);
+    }
+
+}
